f1432cbdc88cda21a0585212dfd63db0c493aa53
DariaDudnik <d.dudnik@dunice.net>
2020-10-12 14:01:19 +0300

    three ways to sum

--- src/three_ways_to_sum_to_n/solution_c.js
+++ src/three_ways_to_sum_to_n/solution_c.js
@@ -3,6 +3,16 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
+// Actually this will cause stack overflow on big numbers
 var sum_to_n = function(n) {
-    // your code here
+    function recursiveSum(currentSum, nextElement) {
+        if (nextElement === BigInt(0)) {
+            return currentSum;
+        }
+        return recursiveSum(currentSum + nextElement, nextElement - BigInt(1));
+    }
+
+    return recursiveSum(BigInt(0), BigInt(n));
 };
+
+console.log(sum_to_n(6));
